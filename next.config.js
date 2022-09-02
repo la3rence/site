31805780d13138bbd76a35f9b74a2c23ea6e3c42
Lawrence Li <lonor@live.com>
2022-09-02 23:01:56 +0800

    feat: test tweet

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
   images: {
-    domains: ["i.picsum.photos", "picsum.photos"],
+    domains: ["i.picsum.photos", "picsum.photos", "pbs.twimg.com"],
   },
 };
--- pages/blog/ssg.js
+++ pages/blog/ssg.js
@@ -1,17 +1,26 @@
 import Blog from "../../components/blog";
 import Image from "next/image";
 import config from "./../../lib/config.json";
+import "react-static-tweets/styles.css";
+import { fetchTweetAst } from "static-tweets";
+import { Tweet } from "react-static-tweets";
+
 export const author = config.authorName;
 export const title = "SSG Example";
 export const date = "2022-08-26";
 
-export default function Example() {
+const tweetId = "1358199505280262150";
+
+export default function Example(props) {
+  const { tweetId, tweetAst } = props;
   return (
     <Blog title={title} date={date} author={author}>
       <p>
         This page is fully rendered by <code>JSX</code>. You can update this
         page with JavaScript and React components.
       </p>
+
+      <Tweet id={tweetId} ast={tweetAst} />
       <div className="-mx-6 filter dark:brightness-50">
         <Image
           src={`https://i.picsum.photos/id/1072/400/400.jpg?grayscale&hmac=CpiwecRoY6G5F_ZTZceMHxJkhHWv24p1ebO5iNvL24M`}
@@ -24,3 +33,19 @@ export default function Example() {
     </Blog>
   );
 }
+
+export const getStaticProps = async () => {
+  try {
+    const tweetAst = await fetchTweetAst(tweetId);
+    return {
+      props: {
+        tweetId,
+        tweetAst,
+      },
+      revalidate: 10,
+    };
+  } catch (err) {
+    console.error("error fetching tweet info", err);
+    throw err;
+  }
+};
