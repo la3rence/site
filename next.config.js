a032986e6c591b550465c0063185ae2697b734ac
Lawrence Li <lonor@live.com>
2022-09-02 21:47:52 +0800

    feat: remove babel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 // next.config.js
 module.exports = {
-  // Prefer loading of ES Modules over CommonJS
-  experimental: { esmExternals: true },
-  webpack: config => {
-    config.experiments = config.experiments || {};
-    config.experiments.topLevelAwait = true;
-    return config;
-  },
   images: {
     domains: ["i.picsum.photos", "picsum.photos"],
   },
