5753c67c9bcfe91f242101644be62f39454a5c29
Lawrence Li <lonor@live.com>
2022-09-04 03:09:10 +0800

    feat: tategaki

--- components/layout.js
+++ components/layout.js
@@ -1,26 +1,30 @@
 import Header from "./header";
 
 export default function Layout(props) {
-  const { children, title } = props;
+  const { children, title, vertical } = props;
   return (
     <>
       <Header title={title} />
       <main
-        className="prose prose-slate
+        className={`prose prose-slate
       max-w-3xl
       dark:prose-invert
       prose-pre:rounded-none 
       prose-pre:bg-[#0d1117] 
-      prose-blockquote:border-purple-500
-      prose-blockquote:text-purple-500
-      prose-blockquote:border-l-2
-      prose-blockquote:not-italic
       prose-p:before:content-none
       prose-headings:[*>a]:no-unnderline
+      prose-blockquote:border-purple-500
+      prose-blockquote:text-purple-500
+      prose-blockquote:not-italic
+      ${
+        vertical
+          ? "prose-blockquote:border-t-2 prose-blockquote:border-l-0 prose-blockquote:pt-4 prose-blockquote:pl-0"
+          : "prose-blockquote:border-l-2"
+      }
       mx-6
-      mb-24"
+      mb-24`}
       >
-        <div className="my-10">{children}</div>
+        <div className="my-6">{children}</div>
       </main>
     </>
   );
