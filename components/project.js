a8b6e07b51a67455fec6f17c7d9b7e7045d9f83f
Lawrence Li <lonor@live.com>
2022-11-26 23:08:18 +0800

    about: delete hover type shit

--- components/project.js
+++ components/project.js
@@ -8,15 +8,7 @@ export default function Project({ name, desc, url }) {
           href={url}
           target="_blank"
         >
-          <h5 className="pt-2">
-            {name}
-            <span
-              className="invisible group-hover:visible absolute z-[1]
-            rounded-lg bg-zinc-300 dark:bg-black text-sm px-2"
-            >
-              {url.replace("https://", "")}
-            </span>
-          </h5>
+          <h5 className="pt-2">{name}</h5>
           <p className="text-sm font-normal">{desc}</p>
         </a>
       )}
