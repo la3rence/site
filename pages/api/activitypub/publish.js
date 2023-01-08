a611d28045bd75faf3bd3b458547dfa235e60b42
Lawrence Li <lonor@live.com>
2023-01-08 15:44:36 +0800

    chore: bug fix

--- pages/api/activitypub/publish.js
+++ pages/api/activitypub/publish.js
@@ -1,7 +1,6 @@
 import { generateNote } from "./outbox";
 import { getMdContentById } from "../../../lib/ssg.mjs";
 import { sendSignedRequest } from "../../../lib/httpSign.mjs";
-import { createElement } from "react";
 
 export default async function publish(req, res) {
   let origin = req.headers.host;
