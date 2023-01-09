402aec6869087cf7e6bb9688b38473ac99d50095
Lawrence Li <lonor@live.com>
2023-01-09 14:41:49 +0800

    activity(wip): fix followers count

--- pages/api/activitypub/followers.js
+++ pages/api/activitypub/followers.js
@@ -3,16 +3,19 @@ import { pushToList, getList } from "../../../lib/redis";
 const FOLLOWERS_KEY = "ap:followers";
 
 export default async function followers(req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/activity+json");
   let origin = req.headers.host;
   origin = origin.includes("localhost")
     ? "http://" + origin
     : "https://" + origin;
+  const followers = await getAllFollowers();
   const response = {
     "@context": "https://www.w3.org/ns/activitystreams",
     id: `${origin}/api/activitypub/followers`,
     type: "OrderedCollection",
     totalItems: followers.length,
-    orderedItems: await getAllFollowers(),
+    orderedItems: followers,
   };
   res.json(response);
 }
