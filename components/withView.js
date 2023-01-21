a19b54fb097900ecd6c750b06f14700f18c30a71
Lawrence Li <lonor@live.com>
2023-01-21 10:17:32 +0800

    chore: switch view api calling with swr

--- components/withView.js
+++ components/withView.js
@@ -1,28 +1,24 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import config from "../lib/config.mjs";
+import useSWR from "swr";
+import { fetcher, swrConfig } from "./loading";
 
 // eslint-disable-next-line react/display-name
 const withView = Component => props => {
   // skip this HOC by: return <Component {...props} />;
   const id = useRouter().asPath.split("?")[0].split("#")[0];
-  const [view, setView] = useState(0);
   const [pageURL, setPageURL] = useState(config.baseURL + id);
+  const { data } = useSWR(`/api/view?page=${id}`, fetcher, swrConfig);
 
   useEffect(() => {
     setPageURL(window.location.href.split("?")[0].split("#")[0]);
-    getViews(id);
   }, [id]);
 
-  const getViews = async id => {
-    const res = await (await fetch(`/api/view?page=${id}`)).json();
-    setView(res.view);
-    console.debug(`page: ${id} view: ${res.view}`);
-  };
   const withViewProps = {
     ...props,
     id,
-    view,
+    view: data ? data.view : 0,
     pageURL,
     domain: new URL(pageURL).hostname,
   };
