13fa936fc898cf7fa6713009ceb5eb81014f1efa
Lawrence Li <lonor@live.com>
2023-03-03 17:26:50 +0800

    like: fix query

--- pages/api/activitypub/like.js
+++ pages/api/activitypub/like.js
@@ -5,8 +5,8 @@ const LIKES_COLLECTION = "likes";
 export const saveLike = async message => {
   const likesCollection = await getCollection(LIKES_COLLECTION);
   const like = {
-    actor: message.actor.id,
-    object: message.object.id,
+    actor: message.actor,
+    object: message.object,
     createdAt: new Date(),
   };
   await likesCollection.insertOne(like);
@@ -16,28 +16,11 @@ export const saveLike = async message => {
 // object: actually the blog url
 export const getLikeForObjectId = async objectId => {
   const likesCollection = await getCollection(LIKES_COLLECTION);
-  const likes = await likesCollection
-    .aggregate([
-      { $match: { object: objectId } },
-      {
-        $lookup: {
-          from: "users",
-          localField: "actor",
-          foreignField: "_id",
-          as: "user",
-        },
-      },
-      {
-        $group: {
-          _id: "$actor",
-          users: { $push: "$user" },
-        },
-      },
-    ])
+  const queryObject = { object: objectId };
+  const projectionObject = { actor: 1 };
+  const result = await likesCollection
+    .find(queryObject, projectionObject)
     .toArray();
-
-  return likes.map(like => ({
-    actor: like._id,
-    users: like.users.flat(),
-  }));
+  console.log(result);
+  return result;
 };
