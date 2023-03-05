3e3e4e8a91992c42dac07b77552c7d81a6a2d6ee
Lawrence Li <lonor@live.com>
2023-03-05 10:42:01 +0800

    ui: add regenerate button

--- pages/ai.js
+++ pages/ai.js
@@ -20,7 +20,7 @@ const Message = ({ role, content, isLoading }) => {
   if (role === "user") {
     return (
       <div
-        className="dark:bg-zinc-900 p-2"
+        className="p-2"
         dangerouslySetInnerHTML={{
           __html: md.render(`○ ${content}`),
         }}
@@ -28,15 +28,11 @@ const Message = ({ role, content, isLoading }) => {
     );
   }
   if (isLoading) {
-    return (
-      <div className="dark:bg-zinc-900">
-        <Lines />
-      </div>
-    );
+    return <Lines />;
   }
   return (
     <div
-      className="dark:bg-zinc-800 py-4 prose-p:p-2 prose-p:my-0 prose-pre:px-6 prose-pre:my-0 prose-pre:break-words"
+      className="py-4 prose-p:p-2 prose-p:my-0 prose-pre:px-6 prose-pre:my-0 prose-pre:break-words"
       dangerouslySetInnerHTML={{
         __html: md.render(`● ${content}`),
       }}
@@ -94,14 +90,22 @@ const Chat = props => {
     chat.push({ role: "assistant", content: result });
     setChat([...chat]);
     inputRef.current.focus();
-    const fly = await fetch(
-      `${process.env.NEXT_PUBLIC_LOG_API}?message=${question}`
-    );
-    await fly.json();
+    if (question) {
+      const fly = await fetch(
+        `${process.env.NEXT_PUBLIC_LOG_API}?message=${question}`
+      );
+      await fly.json();
+    }
   };
 
   const clear = () => {
     setChat([]);
+  };
+
+  const regenerate = async () => {
+    setIsLoading(true);
+    chat.pop();
+    await answer();
   };
 
   return (
@@ -120,6 +124,24 @@ const Chat = props => {
           />
         );
       })}
+      {chat.length > 1 && !isLoading && (
+        <div className="flex">
+          <div className="flex-1"></div>
+          <div className="p-1 h-6 w-6 mr-4 cursor-pointer" onClick={regenerate}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 32 32"
+            >
+              <path
+                d="M25.95 7.65l.005-.004c-.092-.11-.197-.206-.293-.312c-.184-.205-.367-.41-.563-.603c-.139-.136-.286-.262-.43-.391c-.183-.165-.366-.329-.558-.482c-.16-.128-.325-.247-.49-.367c-.192-.14-.385-.277-.585-.406a13.513 13.513 0 0 0-.533-.324q-.308-.179-.625-.341c-.184-.094-.37-.185-.56-.27c-.222-.1-.449-.191-.678-.28c-.19-.072-.378-.145-.571-.208c-.246-.082-.498-.15-.75-.217c-.186-.049-.368-.102-.556-.143c-.29-.063-.587-.107-.883-.15c-.16-.023-.315-.056-.476-.073A12.933 12.933 0 0 0 6 7.703V4H4v8h8v-2H6.811A10.961 10.961 0 0 1 16 5a11.111 11.111 0 0 1 1.189.067c.136.015.268.042.403.061c.25.037.501.075.746.128c.16.035.315.08.472.121c.213.057.425.114.633.183c.164.054.325.116.486.178c.193.074.384.15.57.235c.162.072.32.15.477.23q.268.136.526.286c.153.09.305.18.453.276c.168.11.33.224.492.342c.14.102.282.203.417.312c.162.13.316.268.47.406c.123.11.248.217.365.332c.167.164.323.338.479.512A10.993 10.993 0 1 1 5 16H3a13 13 0 1 0 22.95-8.35z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      )}
       {isLoading && <Message role={"assistant"} isLoading={true} />}
       <div ref={bottomRef} id="input" className="mt-20">
         <div className="flex">
