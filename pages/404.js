dad9952e3171f024db9e143a586ee3e785574167
Lawrence Li <lonor@live.com>
2023-03-11 13:16:17 +0800

    feat: custom 404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <div className="flex justify-center items-center mt-24">
+        404 - Page Not Found
+      </div>
+    </Layout>
+  );
+}
