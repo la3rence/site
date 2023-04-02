ea728168bcaa0e4a4f7c9578c3a153b45ba1e810
Lawrence Li <lonor@live.com>
2023-04-02 17:44:51 +0800

    feat: add build timestamp and commit hash (#165)

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,10 @@ import config from "../lib/config.mjs";
 import { useEffect, useState } from "react";
 import { TwitterIcon, GitHubIcon, MailIcon } from "./svg";
 
-let { siteTitle, twitter, github, authorEmail } = config;
+const { siteTitle, twitter, github, repo, authorEmail, enableBuildInfo } =
+  config;
+const BUILDTIME = process.env.NEXT_PUBLIC_BUILDTIME;
+const GITSHA = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA; // latest git commit sha provided from vercel
 
 const Footer = () => {
   const [mounted, setMounted] = useState(false);
@@ -17,7 +20,7 @@ const Footer = () => {
   }
 
   return (
-    <div className="mx-4 mt-16 mb-24 text-sm text-center">
+    <div className="mx-4 mt-16 mb-24 text-xs text-center">
       {twitter && (
         <div className="px-1 inline-block">
           <A href={`https://twitter.com/${twitter}`}>
@@ -45,6 +48,16 @@ const Footer = () => {
           <span className="underline hover:text-zinc-500">{siteTitle}</span>
         </A>
       </div>
+      {enableBuildInfo && (
+        <div className="mt-2">
+          <span className="mx-1 text-zinc-500 dark:text-zinc-600">
+            Built on {new Date(BUILDTIME * 1000).toLocaleString()}
+          </span>
+          <A href={`https://github.com/${github}/${repo}/commit/${GITSHA}`}>
+            <span className="">{GITSHA?.slice(0, 6)}</span>
+          </A>
+        </div>
+      )}
     </div>
   );
 };
