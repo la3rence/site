0a68af4a0714b103e75867755201830a02d7c5c7
Lawrence Li <lonor@live.com>
2023-04-23 18:40:51 +0800

    style: re-render after resizing

--- components/pixi.js
+++ components/pixi.js
@@ -3,43 +3,55 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function GraphicsExample() {
   const [loaded, setLoaded] = useState(false);
-  useEffect(() => {
-    setLoaded(true);
-  });
-  const draw = useCallback(g => {
+  const [windowWidth, setWindowWidth] = useState(768);
+  let offset = 300;
+  const draw = useCallback((g, offset) => {
     g.clear();
     g.beginFill(0x000000);
     g.lineStyle(4, 0x000000, 1);
-    g.moveTo(250, 50);
-    g.lineTo(190, 150);
-    g.lineTo(310, 150);
+    g.moveTo(offset + 250, 50);
+    g.lineTo(offset + 190, 150);
+    g.lineTo(offset + 310, 150);
     g.endFill();
     g.lineStyle(2, 0x000000, 1);
     g.beginFill(0x000000, 1);
-    g.drawRect(350, 50, 100, 100);
+    g.drawRect(offset + 350, 50, 100, 100);
     g.endFill();
     g.lineStyle(0);
     g.beginFill(0x000000, 0.9);
-    g.drawCircle(100, 100, 55);
+    g.drawCircle(offset + 100, 100, 55);
     g.endFill();
   }, []);
+
+  useEffect(() => {
+    setLoaded(true);
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    // console.debug(windowWidth);
+    if (windowWidth < 700) {
+      offset = 0;
+    }
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth]);
 
   if (!loaded) return <div className="h-[200px] w-full"></div>;
 
   return (
     <div className="h-[200px]">
-      <div className="absolute left-0 w-full flex items-center justify-center p-0 ">
-        <div className="w-1/3 h-[200px] bg-[#eee] "></div>
+      <div className="absolute left-0 w-full">
         <div className="overflow-x-auto">
           <Stage
-            width={768}
+            width={window.innerWidth}
             height={200}
             options={{ backgroundColor: 0xeeeeee }}
           >
-            <Graphics draw={draw} />
+            <Graphics draw={g => draw(g, offset)} />
           </Stage>
         </div>
-        <div className="w-1/3 h-[200px] bg-[#eee]"></div>
       </div>
     </div>
   );
