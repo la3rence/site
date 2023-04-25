e4843f574759676aea612b7d209a5aed96ffdd48
Lawrence Li <lonor@live.com>
2023-04-25 21:30:22 +0800

    feat: add react spring

--- pages/blog/example.js
+++ pages/blog/example.js
@@ -1,23 +1,47 @@
 import Blog from "../../components/blog";
 import Image from "next/image";
-import config from "../../lib/config.mjs";
+import cfg from "../../lib/config.mjs";
 import Bilibili from "../../components/bilibili";
 import Douban from "../../components/douban";
 import GitHub from "../../components/github";
 import demoPic from "../../public/images/the1975.jpg";
+import { useSpring, animated } from "@react-spring/web";
 
 export const blogProps = {
-  author: config.authorName,
+  author: cfg.authorName,
   title: "Example",
   date: "2022-08-26",
   someKey: "someValeInJSXProps",
-  tags: "Example, Test, Video, Douban",
-  visible: false,
+  tags: "example, test, jsx",
+  visible: true,
 };
 
 export default function Example() {
+  const springs = useSpring({
+    from: { background: "#ff6d6d", y: 0, x: -100 },
+    to: [
+      { x: -50, background: "#fff59a" },
+      { x: 0, background: "#88DFAB" },
+      { x: 50, background: "#569AFF" },
+      { x: 100, background: "#ff6d6d" },
+    ],
+    loop: true,
+  });
+
   return (
     <Blog {...blogProps}>
+      <h2>Animation</h2>
+      <div className="w-full flex items-center justify-center">
+        <animated.div
+          style={{
+            width: 40,
+            height: 40,
+            // background: '#222222',
+            borderRadius: 6,
+            ...springs,
+          }}
+        />
+      </div>
       <p>
         This page is fully rendered by <code>JSX</code>. You can update this
         page with JavaScript and React components.
