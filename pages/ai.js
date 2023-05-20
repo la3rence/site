cf21e1db032ca3322a9608f24df1e15f08bbefa2
Lawrence Li <lonor@live.com>
2023-05-20 10:57:50 +0800

    page: refactor with new sse

--- pages/ai.js
+++ pages/ai.js
@@ -3,6 +3,11 @@ import Blog from "../components/blog";
 import MarkdownIt from "markdown-it";
 import hljs from "highlight.js";
 import { Lines } from "../components/loading";
+import { v4 as uuidv4 } from "uuid";
+import { fetchEventSource } from "@microsoft/fetch-event-source";
+
+let parentMessageId = null;
+let conversationId = null;
 
 const md = new MarkdownIt({
   highlight: function (str, lang) {
@@ -74,47 +79,73 @@ const Chat = props => {
   const answer = async question => {
     setChat([...chat]);
     setIsLoading(true);
-    let res;
+    let currentData = "";
     try {
-      res = await fetch(process.env.NEXT_PUBLIC_CHAT_API, {
+      const body = {
+        messages: [
+          {
+            id: uuidv4(),
+            author: {
+              role: "user",
+            },
+            content: {
+              parts: [question],
+              content_type: "text",
+            },
+            create_time: (Date.now() / 1000).toFixed(7),
+          },
+        ],
+        parent_message_id: parentMessageId ? parentMessageId : uuidv4(),
+        model: "text-davinci-002-render-sha-mobile",
+        action: "next",
+      };
+      if (conversationId) {
+        body.conversation_id = conversationId;
+      }
+      chat.push({ role: "assistant", content: "" });
+      await fetchEventSource(process.env.NEXT_PUBLIC_CHAT_API, {
         method: "POST",
-        body: JSON.stringify({
-          customKey: "",
-          messages: chat,
-        }),
+        mode: "cors",
+        headers: {
+          "Content-Type": "text/event-stream",
+          "oai-client-type": "ios",
+        },
+        body: JSON.stringify(body),
+        onmessage(event) {
+          if (event.data === "[DONE]") {
+            return;
+          }
+          chat.pop();
+          setChat([...chat]);
+          const data = JSON.parse(event.data);
+          console.debug("sse onmessage", event.data);
+          currentData = data.message?.content?.parts?.[0];
+          conversationId = data.conversation_id;
+          parentMessageId = data.message.id;
+          chat.push({
+            role: "assistant",
+            content: currentData + "●",
+          });
+          setChat([...chat]);
+          bottomRef.current.scrollIntoView({ behavior: "smooth" });
+        },
+        onerror(error) {
+          throw error;
+        },
+        async onclose() {
+          console.debug("sse closed");
+          chat.pop();
+          setAssistantChat(currentData);
+          const fly = await fetch(
+            `${process.env.NEXT_PUBLIC_LOG_API}?message=${question}`
+          );
+          const res = await fly.json();
+          console.debug(res);
+        },
       });
     } catch (error) {
       setAssistantChat(error);
       return;
-    }
-    const data = res.body;
-    const reader = data.getReader();
-    const decoder = new TextDecoder("utf-8");
-    let done = false;
-    let currentData = "";
-    while (!done) {
-      const { value, done: readerDone } = await reader.read();
-      if (value) {
-        let char = decoder.decode(value);
-        if (char === "\n" && currentData.endsWith("\n")) {
-          continue;
-        }
-        if (char) {
-          currentData += char;
-          chat.push({ role: "assistant", content: currentData });
-          setChat([...chat]);
-          chat.pop();
-          bottomRef.current.scrollIntoView({ behavior: "smooth" });
-        }
-      }
-      done = readerDone;
-    }
-    setAssistantChat(currentData);
-    if (question) {
-      const fly = await fetch(
-        `${process.env.NEXT_PUBLIC_LOG_API}?message=${question}`
-      );
-      await fly.json();
     }
   };
 
@@ -130,14 +161,14 @@ const Chat = props => {
 
   const regenerate = async () => {
     chat.pop();
-    await answer();
+    await answer(chat.slice(-1)[0].content);
   };
 
   return (
     <Blog
       noMeta
       // noFooter
-      title={`֎ ${q ? q : ""}`}
+      title={`${q ? q : ""}`}
       description="Get instant answers, explanations, and examples for all of your questions."
     >
       {chat.map((messageObj, index) => {
