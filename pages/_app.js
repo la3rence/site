cbb3e8609648440b8a895e22b3eb309467024b71
Lawrence Li <lonor@live.com>
2023-08-03 20:24:02 +0800

    chore: remove client side auth and ai page

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,18 @@
 import App from "next/app";
-import { Analytics } from "@vercel/analytics/react";
 import Umami from "../components/analytics";
 import { ThemeProvider } from "next-themes";
 import "./../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
 
 class Site extends App {
   render() {
-    const { Component, pageProps, session } = this.props;
+    const { Component, pageProps } = this.props;
     return (
-      <SessionProvider session={session}>
+      <>
         <ThemeProvider storageKey="theme" attribute="class">
           <Component {...pageProps} />
         </ThemeProvider>
         <Umami />
-        <Analytics />
-      </SessionProvider>
+      </>
     );
   }
 }
--- pages/blog/example.js
+++ pages/blog/example.js
@@ -5,7 +5,6 @@ import Bilibili from "../../components/bilibili";
 import Douban from "../../components/douban";
 import GitHub from "../../components/github";
 import demoPic from "../../public/images/the1975.jpg";
-import { useSpring, animated } from "@react-spring/web";
 
 export const blogProps = {
   author: cfg.authorName,
@@ -21,31 +20,8 @@ export const blogProps = {
  * You can use any client side React component in it.
  */
 export default function Example() {
-  const springs = useSpring({
-    from: { background: "#ff6d6d", y: 0, x: -100 },
-    to: [
-      { x: -50, background: "#fff59a" },
-      { x: 0, background: "#88DFAB" },
-      { x: 50, background: "#569AFF" },
-      { x: 100, background: "#ff6d6d" },
-    ],
-    loop: true,
-  });
-
   return (
     <Blog {...blogProps}>
-      <h2>Animation</h2>
-      <div className="w-full flex items-center justify-center">
-        <animated.div
-          style={{
-            width: 40,
-            height: 40,
-            // background: '#222222',
-            borderRadius: 6,
-            ...springs,
-          }}
-        />
-      </div>
       <p>
         This page is fully rendered by <code>JSX</code>. You can update this
         page with JavaScript and React components.
