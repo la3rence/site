725218fcbc34e93e45f27b8e161cdbee5e34bc5e
Lawrence Li <lonor@live.com>
2023-08-23 19:09:53 +0800

    chore: add env

--- components/analytics.js
+++ components/analytics.js
@@ -2,8 +2,16 @@ export default function Umami() {
   return (
     <script
       async
-      src="https://analytics.lawrenceli.me/script.js"
+      src={process.env.NEXT_PUBLIC_ANALYTICS_URL}
       data-website-id="3ac93385-5b59-43e3-9b02-01fe09804587"
     ></script>
   );
 }
+
+export const Adsense = () => (
+  <script
+    async
+    src={process.env.NEXT_PUBLIC_ADS_URL}
+    crossorigin="anonymous"
+  ></script>
+);
