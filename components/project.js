2eab1bf955c4646b414d320365271061c213b069
lawrence.li <hi@lawrenceli.me>
2024-05-12 15:10:58 +0800

    chore: style changes

--- components/project.js
+++ components/project.js
@@ -3,14 +3,14 @@ export default function Project({ name, desc, desc_zh, url, locale }) {
     <>
       {name && (
         <a
-          className="group cursor-pointer no-underline sm:rounded-lg bg-zinc-100 
+          className="group cursor-pointer no-underline bg-zinc-100 
        hover:bg-zinc-200 sm:w-[49%] w-full dark:bg-zinc-800 dark:hover:bg-zinc-600 shadow"
           href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h5 className="pt-2">{name}</h5>
-          {locale === "zh" && <p className="text-sm font-normal">{desc_zh}</p>}
+          {locale === "zh" && <p className="text-sm my-2 mx-4  p-0 font-normal">{desc_zh}</p>}
           {locale != "zh" && <p className="text-sm font-normal">{desc}</p>}
         </a>
       )}
