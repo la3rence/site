5c2435ca44cbb2b8b746f0578cc240f47ecb51a4
lawrence.li <hi@lawrenceli.me>
2024-07-20 18:02:38 +0800

    fix: activitypub json for default locale

--- pages/api/activitypub/blog/[id].js
+++ pages/api/activitypub/blog/[id].js
@@ -1,11 +1,15 @@
 import { getOrigin, respondActivityJSON } from "../../../../lib/util.js";
 import { getMdContentById } from "../../../../lib/ssg.mjs";
 import { generateNote } from "../outbox.js";
+import config from "../../../../lib/config.mjs";
 
 export default async function Blog(req, res) {
   const origin = getOrigin(req);
   const { id } = req.query;
-  const post = await getMdContentById(id);
+  let post = await getMdContentById(id);
+  if (!post) {
+    post = await getMdContentById(`${id}.${config.defaultLocale}`);
+  }
   const note = generateNote(origin, post);
   respondActivityJSON(res, note);
 }
