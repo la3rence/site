fdd21268d4c83134fbf6528b3781b3b20a762530
lawrence <hi@lawrenceli.me>
2025-07-26 11:53:17 +0800

    feat: middleware refinement

--- middleware.js
+++ middleware.js
@@ -5,15 +5,19 @@ import cfg from "./lib/config.mjs";
 export function middleware(req) {
   const path = req.nextUrl.pathname;
   // ActivityPub Accept Rewrite
-  const accept = req.headers.get("Accept");
-  if (path.startsWith("/blog") && accept && accept.includes("activity")) {
-    const blogId = path.split("/")[2];
-    return NextResponse.rewrite(new URL(`/api/activitypub/blog/${blogId}`, req.url));
+  if (path.startsWith("/blog")) {
+    const accept = req.headers.get("Accept");
+    if (accept && accept.includes("activity")) {
+      const blogId = path.split("/")[2];
+      return NextResponse.rewrite(new URL(`/api/activitypub/blog/${blogId}`, req.url));
+    }
   }
   // RSS Feed i18n
   if (path.endsWith(cfg.feedFile)) {
     const locale = req.nextUrl.locale;
-    return NextResponse.rewrite(new URL(`/atom.${locale}.xml`, req.url));
+    const res = NextResponse.rewrite(new URL(`/atom.${locale}.xml`, req.url));
+    res.headers.set("Cache-Control", "public, max-age=3600, stale-while-revalidate=43200");
+    return res;
   }
 }
 
