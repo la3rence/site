688b04bf38c231760c20feb5f258a366c022a5aa
lawrence <hi@lawrenceli.me>
2026-04-23 17:37:06 +0800

    perf: remove index prefetch

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,33 @@
 import Link from "next/link";
 import Layout from "./../components/layout";
-import { useRouter } from "next/router";
 import { getAllPostIndexData } from "../lib/ssg";
 
 const getYear = date => new Date(date).getFullYear();
 
-const Index = function index({ allPostsData }) {
-  const aggregatedPosts = allPostsData.reduce((result, currentPost) => {
-    (result[getYear(currentPost.date)] = result[getYear(currentPost.date)] || []).push(currentPost);
-    return result;
-  }, {});
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const router = useRouter();
+const Index = function index({ postsByYear }) {
   return (
     <Layout blogIndex title={"Blog"} tags={"Blog"}>
       <div className="blogIndex mt-8">
-        {Object.keys(aggregatedPosts)
-          .sort((a, b) => b - a)
-          .map(year => {
-            return (
-              <div key={year}>
-                {aggregatedPosts[year].some(post => post.locale === router.locale) && (
-                  <div className="text-zinc-500 text-2xl mt-12 mb-6" id={year}>
-                    {year}
-                  </div>
-                )}
-                {aggregatedPosts[year]
-                  .filter(post => post.locale === router.locale)
-                  .map(post => (
-                    <div className="my-4" key={post.id}>
-                      <Link
-                        href={`/blog/${post.id}`}
-                        locale={post.locale}
-                        className={`text-lg font-normal no-underline hover:text-zinc-500`}
-                      >
-                        <span>{post.title}</span>
-                        <span className="font-mono text-base text-zinc-500 float-right">
-                          {post.date}
-                        </span>
-                      </Link>
-                    </div>
-                  ))}
+        {postsByYear.map(({ year, posts }) => (
+          <div key={year}>
+            <div className="text-zinc-500 text-2xl mt-12 mb-6" id={year}>
+              {year}
+            </div>
+            {posts.map(post => (
+              <div className="my-4" key={post.id}>
+                <Link
+                  href={`/blog/${post.id}`}
+                  locale={post.locale}
+                  prefetch={false}
+                  className={`text-lg font-normal no-underline hover:text-zinc-500`}
+                >
+                  <span>{post.title}</span>
+                  <span className="font-mono text-base text-zinc-500 float-right">{post.date}</span>
+                </Link>
               </div>
-            );
-          })}
+            ))}
+          </div>
+        ))}
       </div>
     </Layout>
   );
@@ -51,16 +35,34 @@ const Index = function index({ allPostsData }) {
 
 export default Index;
 
-export const getStaticProps = async () => {
-  const allPostsData = (await getAllPostIndexData()).map(post => ({
-    id: post.id,
-    title: post.title,
-    date: post.date,
-    locale: post.locale ? post.locale : null,
-  }));
+export const getStaticProps = async ({ locale }) => {
+  const postsByYearMap = (await getAllPostIndexData())
+    .filter(post => post.locale === locale)
+    .reduce((result, post) => {
+      const year = getYear(post.date);
+      const posts = result[year] || [];
+
+      posts.push({
+        id: post.id,
+        title: post.title,
+        date: post.date,
+        locale: post.locale ? post.locale : null,
+      });
+
+      result[year] = posts;
+      return result;
+    }, {});
+
+  const postsByYear = Object.keys(postsByYearMap)
+    .sort((a, b) => b - a)
+    .map(year => ({
+      year,
+      posts: postsByYearMap[year],
+    }));
+
   return {
     props: {
-      allPostsData,
+      postsByYear,
     },
   };
 };
